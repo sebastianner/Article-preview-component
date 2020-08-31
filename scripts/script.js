b705ed2fc1ec864e2d14433761fda2df43314dd4
Sebastian Mera <sebastianner23@gmail.com>
2020-08-31 00:15:30 -0500

    almost done

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,30 @@
-window.onload = function() {
-    window.addEventListener('resize',init, false)
-}
+var myWidth = window.innerWidth
 
-function init() {
-    var myWidth = window.innerWidth
+function movile() {
     var profile = document.getElementById('profile')
     var shr = document.getElementById('share-links')
     var btn_1 = document.getElementById('btn-1')
     var btn_2 = document.getElementById('btn-2')
     btn_1.addEventListener('click', function(){
-        if (myWidth<=900){
+        if (myWidth){
             profile.classList.add('hidde')
             shr.classList.replace('hidde','show')
         }
     })
     btn_2.addEventListener('click', function(){
-        if (myWidth<=900){
+        if (myWidth){
             shr.classList.replace('show','hidde')
             profile.classList.remove('hidde')
         }
     })
 }
 
+function desktop() {
+    
+}
+
+if(myWidth<768) {
+    movile()
+}else{
+    desktop()
+}
