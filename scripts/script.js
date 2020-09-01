ed98ed19b713f853a066bad8f6eadb20ad96afa5
Sebastian Mera <sebastianner23@gmail.com>
2020-08-31 19:18:49 -0500

    project finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,20 @@
-var myWidth = window.innerWidth
-var profile = document.getElementById('profile')
-var shr_mvl = document.getElementById('share-movile')
-var shr_dkt = document.getElementById('share-desktop')
-var btn_1_svg = document.getElementById('btn-1-svg')
 var btn_1 = document.getElementById('btn-1')
 var btn_2 = document.getElementById('btn-2')
+var share_icon = document.getElementById('share-icon')
 
-function movile() {
-    btn_1.addEventListener('click', ()=>{
-            profile.classList.add('hide')
-            shr_mvl.classList.replace('hide','show')
-    })
-    btn_2.addEventListener('click', function(){
-            shr_mvl.classList.replace('show','hide')
-            profile.classList.remove('hide')
-    })
-}
-
-function desktop() {
+function show() {
     btn_1.addEventListener('click',()=>{
-        if (shr_dkt.style.display==='none') {
-            shr_dkt.style.display = 'flex'
+        if (share_icon.style.visibility==='hidden') {
+            share_icon.style.visibility = 'visible'
         }else{
-            shr_dkt.style.display = 'none'
+            share_icon.style.visibility = 'hidden'
         }
     })
+        btn_2.addEventListener('click',()=>{
+            if (share_icon.style.visibility==='visible') {
+                share_icon.style.visibility = 'hidden'
+            }
+    })
 }
 
-if (myWidth<935) {
-    movile()
-} else {
-    desktop()
-}
+show()
